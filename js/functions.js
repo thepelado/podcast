c5662ec2e04d7b4382b02678f6e0705978b630a0
Cristian Brunacci <brunacci.cristian@gmail.com>
2020-02-10 09:24:17 -0300

    Sass and Favicon

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+            $(this).find("a").html("<i class='fas fa-bars'></i>");
+        } else {
+            $(".item").addClass("active");
+            $(this).find("a").html("<i class='fas fa-times'></i>");
+        }
+    });
+
+    $(".item").on("click", function() {
+        if ($(".item").hasClass("active"))
+        {
+            $(".item").removeClass("active");
+            $(".toggle").find("a").html("<i class='fas fa-bars'></i>");
+        }
+    });
+});
